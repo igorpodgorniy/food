577b501573b39698a82d88283489557a9bc6da93
Igor Podgorniy <ipod-92@mail.ru>
2021-03-10 09:29:37 +0300

    Добавил точки для навигации по слайдам карусели.

--- js/script.js
+++ js/script.js
@@ -273,8 +273,8 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     // Slider
-
-    const slidesWrapper = document.querySelector('.offer__slider-wrapper');
+    const slider = document.querySelector('.offer__slider');
+    const slidesWrapper = slider.querySelector('.offer__slider-wrapper');
     const slides = slidesWrapper.querySelectorAll('.offer__slide');
     const slidesField = slidesWrapper.querySelector('.offer__slider-inner');
     const prev = document.querySelector('.offer__slider-prev');
@@ -302,7 +302,25 @@ window.addEventListener('DOMContentLoaded', () => {
 
     slides.forEach(slide => {
         slide.style.width = width;
-    })
+    });
+
+    slider.style.position = 'relative';
+
+    const dotsNav = document.createElement('ol');
+    const dotsArr = [];
+    dotsNav.classList.add('carousel-indicators');
+    slider.append(dotsNav);
+
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('li');
+        dot.setAttribute('data-slide-to', i + 1);
+        dot.classList.add('dot');
+        if (i === 0) {
+            dot.style.opacity = 1;
+        }
+        dotsNav.append(dot);
+        dotsArr.push(dot);
+    }
 
     next.addEventListener('click', () => {
         if (offset === +width.slice(0, width.length - 2) * (slides.length - 1)) {
@@ -325,6 +343,9 @@ window.addEventListener('DOMContentLoaded', () => {
             sliderCountTotal.textContent = slides.length;
             sliderCurrent.textContent = slideIndex;
         }
+
+        dotsArr.forEach(dot => dot.style.opacity = '.5');
+        dotsArr[slideIndex - 1].style.opacity = 1;
     });
 
     prev.addEventListener('click', () => {
@@ -348,5 +369,30 @@ window.addEventListener('DOMContentLoaded', () => {
             sliderCountTotal.textContent = slides.length;
             sliderCurrent.textContent = slideIndex;
         }
+
+        dotsArr.forEach(dot => dot.style.opacity = '.5');
+        dotsArr[slideIndex - 1].style.opacity = 1;
+    });
+
+    dotsArr.forEach(dot => {
+        dot.addEventListener('click', e => {
+            const slideTo = e.target.getAttribute('data-slide-to');
+
+            slideIndex = slideTo;
+            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+
+            slidesField.style.transform = `translateX(-${offset}px)`;
+
+            if (slides.length < 10) {
+            sliderCountTotal.textContent = `0${slides.length}`;
+            sliderCurrent.textContent = `0${slideIndex}`;
+        } else {
+            sliderCountTotal.textContent = slides.length;
+            sliderCurrent.textContent = slideIndex;
+        }
+
+            dotsArr.forEach(dot => dot.style.opacity = '.5');
+            dotsArr[slideIndex - 1].style.opacity = 1;
+        });
     });
 });
