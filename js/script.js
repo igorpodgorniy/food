ccfed5f6539065d1a9b73e6c3086a62bcf89d93a
Igor Podgorniy <ipod-92@mail.ru>
2021-03-11 12:14:49 +0300

    Заменил метод slice на replace с регулярным выражением. Вынес преобразование строки ширины слайдера в число в отдельную функцию.

--- js/script.js
+++ js/script.js
@@ -273,6 +273,7 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     // Slider
+
     const slider = document.querySelector('.offer__slider');
     const slidesWrapper = slider.querySelector('.offer__slider-wrapper');
     const slides = slidesWrapper.querySelectorAll('.offer__slide');
@@ -296,17 +297,13 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
     
-    function counterSlideIndex() {
-        if (slideIndex == slides.length) {
-            slideIndex = 1;
-        } else {
-            slideIndex++;
-        }
-    }
-
     function activeDot() {
         dotsArr.forEach(dot => dot.style.opacity = '.5');
         dotsArr[slideIndex - 1].style.opacity = 1;
+    }
+
+    function cutNum(str) {
+        return +str.replace(/\D/g, '');
     }
 
     formatSlideIndex();
@@ -340,27 +337,39 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     next.addEventListener('click', () => {
-        if (offset === +width.slice(0, width.length - 2) * (slides.length - 1)) {
+        if (offset == cutNum(width) * (slides.length - 1)) {
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += cutNum(width);
         }
+
         slidesField.style.transform = `translateX(-${offset}px)`;
 
-        counterSlideIndex();
+        if (slideIndex == slides.length) {
+            slideIndex = 1;
+        } else {
+            slideIndex++;
+        }
+
         formatSlideIndex();
         activeDot();
     });
 
     prev.addEventListener('click', () => {
         if (offset === 0) {
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            offset = cutNum(width) * (slides.length - 1);
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= cutNum(width);
         }
+
         slidesField.style.transform = `translateX(-${offset}px)`;
 
-        counterSlideIndex();
+        if (slideIndex == 1) {
+            slideIndex = slides.length;
+        } else {
+            slideIndex--;
+        }
+
         formatSlideIndex();
         activeDot();
     });
@@ -370,7 +379,7 @@ window.addEventListener('DOMContentLoaded', () => {
             const slideTo = e.target.getAttribute('data-slide-to');
 
             slideIndex = slideTo;
-            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+            offset = cutNum(width) * (slideTo - 1);
 
             slidesField.style.transform = `translateX(-${offset}px)`;
 
